749e688155e6ae92942a7988a6377f19294fe613
Oscar Olsson <osse.olsson@gmail.com>
2019-03-14 12:36:25 +0100

    rel 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DieScheiteApi.Mixfile do
   def project do
     [
       app: :die_scheite_api,
-      version: "0.3.3",
+      version: "0.4.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix] ++ Mix.compilers,
